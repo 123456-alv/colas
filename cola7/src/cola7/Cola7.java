fd0007049d377eb9a14426a82bbf68fc8b1e93ad
123456-alv <62567116+123456-alv@users.noreply.github.com>
2020-06-05 20:39:59 -0500

    Add files via upload

---
+++ cola7/src/cola7/Cola7.java
@@ -0,0 +1,61 @@
+package cola7;
+import java.util.Scanner;
+
+public class Cola7 {
+int g;
+int A;
+String v1;
+String nom[];
+ String[] nombre=new String[4];
+    public void vector(){
+       System.out.println("ingresa el tamaño del arreglo de 1 a 4");
+        Scanner Teclado = new Scanner(System.in);
+        A = Teclado.nextInt();
+        
+       nom = new String[A];
+}
+    
+    public void llenarcola(){
+        Scanner Teclado = new Scanner(System.in);
+        System.out.println("ingresar nombre");
+        v1 = Teclado.nextLine();
+        nom[g++] = v1;
+ for(int i=0; i<A;i++){
+            System.out.println("valor de la pocison es "+nom[i]);
+        } 
+    }
+
+
+    
+    public void eliminarcola(){
+        A--;
+        System.out.println("elemento borrado");
+    }
+    public static void main(String[] args){
+     
+        Cola7 c = new Cola7();
+        Scanner sc = new Scanner(System.in);
+         int opt=0; 
+         do{
+             System.out.println("1 tamaño del vector");
+             System.out.println("2 ingresar nombres");
+             System.out.println("3 eliminar nombre");
+             System.out.println("4 salir");
+           switch(opt= sc.nextInt()){
+               case 1: 
+                   if(opt<'4'){
+                       
+                       c.vector();
+                   }
+                   break;
+               
+               case 2:
+                   c.llenarcola();
+                   break;
+               case 3:
+                   c.eliminarcola();
+                   break;
+           }
+         }while(opt !=4);
+    }
+}
